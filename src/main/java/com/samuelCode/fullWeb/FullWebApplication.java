87f9438dd289f7ab6c0f24d3ce0881adf4b7b51e
Samuel Kunle <samuelkunle215@gmail.com>
2024-05-24 21:41:37 +0100

    my first commit

---
+++ src/main/java/com/samuelCode/fullWeb/FullWebApplication.java
@@ -0,0 +1,13 @@
+package com.samuelCode.fullWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuelCode/fullWeb/FullWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelCode.fullWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
