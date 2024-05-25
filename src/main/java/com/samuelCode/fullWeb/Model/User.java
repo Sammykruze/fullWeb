dd135b9e411d2b0026dc9cb248eb02eb6b48a184
Samuel Kunle <samuelkunle215@gmail.com>
2024-05-25 22:11:22 +0100

    Creating user model

---
+++ src/main/java/com/samuelCode/fullWeb/Model/User.java
@@ -0,0 +1,47 @@
+package com.samuelCode.fullWeb.Model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String eMail;
+
+    public long getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public User(String firstName, String lastName, String eMail) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eMail = eMail;
+    }
+}
